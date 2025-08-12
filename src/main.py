8a78ff29850ec766e10b2ea04b26e14e7afc7ad1
gzhdev <gzhdev@outlook.com>
2025-08-12 19:47:08 +0800

    v2

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from math import floor
 from time import sleep
 
@@ -5,6 +7,8 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import logging
 
@@ -19,10 +23,19 @@ logger.addHandler(handler)
 # 常量区
 VIDEO_SPEED = 1.0  # 视频播放速度
 
+def resource_path(relative_path):
+    """ 获取资源的绝对路径，无论是开发环境还是打包环境 """
+    try:
+        # PyInstaller 创建一个临时文件夹，并把路径存储在 _MEIPASS 中
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 def init_driver(options: Options = None):
     options = Options() if options is None else options
-    service = Service(executable_path=r"../driver/chromedriver.exe")
+    service = Service(executable_path="chromedriver.exe")
 # options = Options()
     driver = webdriver.Chrome(service=service, options=options)
     return driver
@@ -32,14 +45,17 @@ def manul_login(driver):
         driver.get("https://pc.kmelearning.com/jsncxyslhs/home/login")
         input("登录后在此回车\n")
         try:
-            dashborad = driver.find_element(By.XPATH, '//*[@id="homeIndex"]/div/div[2]/div/div/div/div/div/div[7]/div/div/div/div/div[2]')
+            # dashborad = driver.find_element(By.XPATH, '//*[@id="homeIndex"]/div/div[2]/div/div/div/div/div/div[7]/div/div/div/div/div[2]')
+            dashboard = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="homeIndex"]/div/div[2]/div/div/div/div/div/div[7]/div/div/div/div/div[2]'))
+            )
         except Exception as e:
             logger.error(f"查找个人中心元素失败: {e}")
             return False
-        if dashborad and dashborad.text == '个人中心':
+        if dashboard and dashboard.text == '个人中心':
             return True
         else:
-            logger.error(f"元素文本不符，实际为: '{dashborad.text}'")
+            logger.error(f"元素文本不符，实际为: '{dashboard.text}'")
             raise Exception("登录页面不正确")
     except Exception as e:
         logger.error(f"登录流程异常: {e}")
@@ -103,6 +119,10 @@ def get_lesson_list(driver):
 def get_video_list(driver):
     try:
         videoList = driver.find_elements(By.CLASS_NAME, 'course-chapters-section')
+        for v in videoList:
+            if v.find_element(By.CLASS_NAME, 'course-chapters-section-name').text == '测试题':
+                videoList.remove(v)
+                logger.info(f'存在测试题')
         if not videoList:
             logger.info('没有课程列表')
         return videoList
@@ -135,13 +155,21 @@ def handle_lesson(driver, lesson):
         videoList = get_video_list(driver)
         logger.info(f"课程包含视频数量: {len(videoList)}")
         for video in videoList:
-            handle_video(driver, video)
+            try:
+                handle_video(driver, video)
+            except Exception as e:
+                logger.error(f'处理视频{video.find_element(By.CLASS_NAME, 'course-chapters-section-name').text}失败: {e}')
         logger.info("课程结束，返回任务列表")
         driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div/div/div[2]/div/div[1]').click()  # 点击返回按钮
         sleep(5)
         logger.info(f"完成课程: {lesson_name}")
     except Exception as e:
         logger.error(f"处理课程失败: {e}")
+        driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div/div/div[2]/div/div[1]').click()  # 点击返回按钮
+        sleep(5)
+        logger.info(f"退出课程")
+
+
 
 def handle_video(driver, video):
     """
@@ -158,10 +186,21 @@ def handle_video(driver, video):
     except NoSuchElementException as nsee:
         try:
             video.click()
-            sleep(5)
-            play_button = driver.find_element(By.CLASS_NAME, 'prism-big-play-btn')
-            play_button.click()
-            sleep(2)
+            # sleep(5)
+            # play_button = driver.find_element(By.CLASS_NAME, 'prism-big-play-btn')
+            play_button = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, 'prism-big-play-btn'))
+            )
+            setSpeed(driver, VIDEO_SPEED)  # 设置视频播放速度
+            # play_button.click()
+            # sleep(2)
+            control_bar_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "prism-controlbar"))
+            )
+            new_style = "position: absolute; left: 0px; bottom: 0px; display: blocked;"
+            driver.execute_script("arguments[0].style.cssText = arguments[1];",
+                                  control_bar_element,
+                                  new_style)
             current_time = driver.find_element(By.CLASS_NAME, 'current-time').text
             duration = driver.find_element(By.CLASS_NAME, 'duration').text
             left_time = time_str_to_seconds(duration) - time_str_to_seconds(current_time)
@@ -183,7 +222,27 @@ def time_str_to_seconds(time_str):
     else:
         return int(parts[0])
 
+def setSpeed(driver, speed):
+    """
+    设置视频播放速度
+    :param driver: 驱动
+    :param speed: 速度
+    :return:
+    """
+    try:
+        # videoPlayer = driver.find_element(By.XPATH, '//*[@id="A8V92H61MO"]/video')\
+        videoPlayer = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "video[src*='kmelearning.com']"))
+        )
+        driver.execute_script("arguments[0].play();", videoPlayer)
+        driver.execute_script(f"arguments[0].playbackRate = {speed};", videoPlayer)
+        # videoPlayer.click()
+        logger.info(f"设置视频播放速度为: {speed}x")
+    except Exception as e:
+        logger.error(f"设置视频播放速度失败: {e}")
+
 def main():
+    global VIDEO_SPEED
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     driver = init_driver(options)
@@ -196,6 +255,8 @@ def main():
         logger.info('没有任务可执行，程序退出')
         return
     tasksInput = input(f'请选择要执行的任务（输入任务名称，多个任务用逗号分隔）:\n{list(tasksDict.keys())}\n')
+    speed = input('请输入视频播放速度（默认1.0）: ')
+    VIDEO_SPEED = float(speed) if speed.strip() else 1.0
     tasks = [task.strip() for task in tasksInput.split(',')]
     for task in tasks:
         tasksDict = get_task_list(driver)
